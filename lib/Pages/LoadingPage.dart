058a35ff1e3817af4a4b884dd0f6426f29805192
Nimr <Nimr.h.sawafta@gmail.com>
2021-03-05 16:29:01 +0100

    Elements

--- lib/Pages/LoadingPage.dart
+++ lib/Pages/LoadingPage.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fst_anti_covid_project/Pages/HomePage.dart';
+import 'package:fst_anti_covid_project/Service/FirebaseApp.dart';
+
+import '../main.dart';
 
 class MyLoadingPage extends StatelessWidget {
   static String route = "/";
+  MyLoadingPage() {
+    MyFirebaseApp.initApp();
+    MyApp.myStateNavigator.currentState.pushNamed(MyUsersPage.route);
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
